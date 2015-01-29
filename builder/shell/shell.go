434028eb7a4d5bbda6506dfbe1cc1c27b6614f25
Claus Albøge <ca@tdchosting.dk>
2015-01-29 13:56:53 +0100

    Minor fixing

--- builder/shell/shell.go
+++ builder/shell/shell.go
@@ -1,12 +1,11 @@
 package shell
 
 type InstanceType struct {
-	ServerName:           string
-	Hypervisor:           string
-	Cpus:                 int
-	Memory:               int
-    DiskSize:             int
-	OsName:               string
-	Network:			  string
+	ServerName           string
+	Hypervisor           string
+	Cpus                 int
+	Memory               int
+    DiskSize             int
+	OsName               string
+	Network 			 string
 }
-
